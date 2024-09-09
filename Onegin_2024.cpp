8a11fe19d4cd56fc8b624758b0026a5b64566295
Трефилов Кирилл <142966225+KirillTFK@users.noreply.github.com>
2024-09-09 12:33:15 +0400

    10iqOnegin
    Добавил текстовый ввод и вывод, наконец-то сортирует всего Онегина.

--- Onegin_2024.cpp
+++ Onegin_2024.cpp
@@ -1,43 +1,60 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 
-int stupid_compare (char* first_str, char* second_str);
+const int Columns = 100;
+const int Max_row = 8000;
+FILE *in = 0, *out = 0;
+
+
+void ZOV (FILE *text, char agitation[]);
+int stupid_compare (const char* first_str, const char* second_str);
 void stupid_copy (char* pathetic_parody, 
                   const char* unique_original); //На одном тесте сработало
 
-void stupid_sort (char Onegin[][40]);
+void stupid_sort (char Onegin[][Columns], int size);
+void check_open  (FILE **ptr, const char *name, const char *mode);
+int  read_file   (FILE *manuscript, char compuscript[][Columns]);
+void print_file  (FILE *encyclopedia, char compuscript[][Columns]);
 
-const int Colums = 50;//Знаю что опечатка, но это тупая версия
-const int Max_row = 40;
 
-int main ()
+
+
+int main (int argc, char* argv[])
 {
-    char Onegin[Colums][Max_row] = {"My uncle, what a worthy man,",
-                                    "Falling ill like that, and dying;",
-                                    "It summons up respect, one can",
-                                    "Admire it, as if he were trying.",
-                                    "Let us all follow his example!",
-                                    "But, God, what tedium to sample",
-                                    "That sitting by the bed all day,",
-                                    "All night, barely a foot away!",
-                                    "And the hypocrisy, demeaning,",
-                                    "Of cosseting one who\'s half alive;",
-                                    "Puffing the pillows, you contrive",
-                                    "To bring his medicine unsmiling,",
-                                    "Thinking with a mournful sigh,",
-                                    "“Why the devil can\'t you die?”’"
-                                                            };
-
-    
-    stupid_sort (Onegin);
-
-    for (int i = 0; i < 14; i++)
+    //char Huge[100000] = {};
+    char Onegin [Max_row][Columns] = {};
+    if (argc < 3)
     {
-        printf ("%s\n", Onegin[i]);
+        printf ("Не обноружины названия файлов ввода и вывода\n");
+        exit (EXIT_FAILURE);
     }
     
+    check_open (&in,  argv[1], "r");
+    check_open (&out, argv[2], "w");
 
+    int size = 0;
+    size = read_file (in, Onegin);
+
+    /*for (int c = 0; c < 3; c++)
+    { 
+        for (int i = 0; i < Columns; i++ )
+        {
+            printf ("%d", Onegin[size-1+c][i]);
+        }
+        printf (" ");   
+    }
+    */
+
+    printf ("Прочитал файл\n");
+
+    stupid_sort (Onegin, size);
+
+    printf ("Отсортировал файл\n");
+    print_file  (out, Onegin);
+    
 }
 
 void stupid_copy (char* pathetic_parody,
@@ -49,16 +66,16 @@ void stupid_copy (char* pathetic_parody,
     {
         pathetic_parody[count] = unique_original[count];
     }
-    for (; count < 40; count++)
+    for (; count < Columns; count++)
     {
         pathetic_parody[count] = '\0';
     }
 }
 
-int stupid_compare (char* first_str, char* second_str)
+int stupid_compare (const char* first_str, const char* second_str)
 {
     int i = 0;
-    for (; first_str[i] !='\0' && second_str[i] == first_str[i];i++)
+    for (;second_str[i] == first_str[i] && first_str[i] != '\0';i++)
     {
         continue;
     }
@@ -67,15 +84,15 @@ int stupid_compare (char* first_str, char* second_str)
 
 }
 
-void stupid_sort (char Onegin[][40])
+void stupid_sort (char Onegin[][Columns], int size)
 {
-    char intermediary[40] = {};
+    char intermediary[Columns] = {};
     
-    for (int c = 14; c > 0; c--)
+    for (; size > 0; size--)
     {   
-        char challanger[40] = {};
+        char challanger[Columns] = {};
         stupid_copy (challanger, Onegin[0]);
-        for (int i = 0; i < c-1; i++)
+        for (int i = 0; i < size-1; i++)
         {
             
             if (stupid_compare (Onegin[i], challanger) > 0)
@@ -88,17 +105,70 @@ void stupid_sort (char Onegin[][40])
             //printf ("challanger: %s, intermedia: %s, Onegin[%d]:%s\n", challanger, 
                     //intermediary, i, Onegin[i]);
         }
-        if (stupid_compare (Onegin[c-1], challanger) > 0)
+        if (stupid_compare (Onegin[size-1], challanger) > 0)
         {    
             stupid_copy (Onegin[0], challanger );
         }
 
         else
         {
-            stupid_copy (Onegin[0], Onegin[c-1]);
-            stupid_copy (Onegin[c-1], challanger);
+            stupid_copy (Onegin[0], Onegin[size-1]);
+            stupid_copy (Onegin[size-1], challanger);
         }
     }
 
     
+}
+
+void check_open (FILE** ptr, const char *name, const char *mode)
+{
+    if ((*ptr = fopen(name, mode)) == 0)
+    {
+        printf ("Не удалось открыть файл\n");
+
+        exit (EXIT_FAILURE);
+    }
+
+    return;
+}
+
+int read_file (FILE *manuscript, char compuscript[][Columns])
+{
+    int i = 0;
+    for (;feof (manuscript) == 0; i++)
+    {
+            fgets (compuscript [i], Columns, manuscript);
+            //fputs (compuscript[i], stdout);
+
+    }
+    stupid_copy (compuscript[i], "END_OF_TEXT");
+    return (i - 1);
+}
+
+void print_file (FILE *encyclopedia, char compuscript[][Columns])
+{
+    const size_t length = strlen("END_OF_TEXT") + 1;
+    for (int i = 0; strncmp (compuscript[i], "END_OF_TEXT", length ) != 0;i++)
+    {
+        fputs (compuscript[i], encyclopedia);
+    }
+}
+
+void ZOV (FILE *text, char agitation[])
+{
+    int ch = 0;
+    int i = 0;
+
+    for (; (ch = getc(text)) != EOF;)
+    {
+        
+        if ( ch == 'Z' || ch == 'z' || ch == 'O' || ch == 'o' || ch == 'V'
+            || ch == 'v' || ch == '\n' || ch == ' ')
+        {
+            agitation[i] = ch;
+            i++;    
+        }
+        //printf ("%c\n", ch);
+
+    }
 }
