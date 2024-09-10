223acc5544c4f7eaf26a8193dba5ca6d364e24fd
Трефилов Кирилл <142966225+KirillTFK@users.noreply.github.com>
2024-09-11 02:05:10 +0400

    20iqOnegin
    Добавил более умную функцию копирования, которая копирует по 8 байтов за раз, хотя часть ее функций пока не нашла применения. Теперь знаки пунктуации не влияют на сортировку и отсутствуют пустые строки + строки с упр. комбинацией для новой страницы  принтера + нет строк начинающихся с двух пробелов. Работает это дело пипец как медленно.

--- Onegin_2024.cpp
+++ Onegin_2024.cpp
@@ -3,22 +3,24 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-
+const int ver_width = 10;
 const int Columns = 100;
 const int Max_row = 8000;
 FILE *in = 0, *out = 0;
 
 
 void ZOV (FILE *text, char agitation[]);
-int stupid_compare (const char* first_str, const char* second_str);
-void stupid_copy (char* pathetic_parody, 
-                  const char* unique_original); //На одном тесте сработало
-
+int stupid_compare (const char* biba, const char* boba);
 void stupid_sort (char Onegin[][Columns], int size);
 void check_open  (FILE **ptr, const char *name, const char *mode);
 int  read_file   (FILE *manuscript, char compuscript[][Columns]);
 void print_file  (FILE *encyclopedia, char compuscript[][Columns]);
-
+int sep_grain_from_chaff (const char *harvest);
+void litter (char *rubbish);
+void copy (char* pathetic_parody, 
+                  const char* unique_original, int Max_char_to_copy); //На одном тесте сработало
+void read_in_array (char *arr);
+void erase_evidence (char* arr,int size);
 
 
 int main (int argc, char* argv[])
@@ -36,8 +38,8 @@ int main (int argc, char* argv[])
 
     int size = 0;
     size = read_file (in, Onegin);
-
-    /*for (int c = 0; c < 3; c++)
+    /*
+    for (int c = 0; c < 3; c++)
     { 
         for (int i = 0; i < Columns; i++ )
         {
@@ -77,15 +79,34 @@ void stupid_copy (char* pathetic_parody,
     }
 }
 
-int stupid_compare (const char* first_str, const char* second_str)
+int stupid_compare (const char* biba, const char* boba)
 {
     int i = 0;
-    for (;second_str[i] == first_str[i] && first_str[i] != '\0';i++)
+    int o = 0;
+    for (; biba[i] != '\0';)
     {
-        continue;
+        if (!(isalpha(biba[i])))
+        {
+            i++;
+            continue;
+        }
+
+        if (!(isalpha(boba[o])))
+        {
+            o++;
+            continue;
+        }
+
+        if (biba[i] !=  boba[o])
+        {
+            //printf ("Значение int i в stupid_compare: %d\n", i);
+            return ( tolower(biba[i]) - tolower(boba[o]));
+        }
+        i++;
+        o++;
     }
-    //printf ("Значение int i в stupid_compare: %d\n", i);
-    return ( first_str[i] - second_str[i]); 
+    
+    return ( tolower(biba[i]) - tolower(boba[o]));
 
 }
 
@@ -96,29 +117,29 @@ void stupid_sort (char Onegin[][Columns], int size)
     for (; size > 0; size--)
     {   
         char challanger[Columns] = {};
-        stupid_copy (challanger, Onegin[0]);
+        copy (challanger, Onegin[0], Columns);
         for (int i = 0; i < size; i++)
         {
             
             if (stupid_compare (Onegin[i], challanger) > 0)
             {
                 //printf ("Зашел в if\n");
-                stupid_copy (intermediary, challanger);
-                stupid_copy (challanger, Onegin[i]);
-                stupid_copy (Onegin[i], intermediary);
+                copy (intermediary, challanger, Columns);
+                copy (challanger, Onegin[i], Columns);
+                copy (Onegin[i], intermediary, Columns);
             }
             //printf ("challanger: %s, intermedia: %s, Onegin[%d]:%s\n", challanger, 
                    //  intermediary, i, Onegin[i]);
         }
         if (stupid_compare (Onegin[size], challanger) > 0)
         {    
-            stupid_copy (Onegin[0], challanger );
+            copy (Onegin[0], challanger, Columns );
         }
 
         else
         {
-            stupid_copy (Onegin[0], Onegin[size]);
-            stupid_copy (Onegin[size], challanger);
+            copy (Onegin[0], Onegin[size], Columns);
+            copy (Onegin[size], challanger, Columns);
         }
     }
 
@@ -139,16 +160,26 @@ void check_open (FILE** ptr, const char *name, const char *mode)
 
 int read_file (FILE *manuscript, char compuscript[][Columns])
 {
+    char buf[Columns] = {};
     int i = 0;
     int ch = 0; 
+    int flag;
     for (; (ch = getc(manuscript))!= EOF; i++)
     {
             ungetc (ch, manuscript);
-            fgets (compuscript [i], Columns, manuscript);
+            fgets (buf, Columns, manuscript);
+            flag = sep_grain_from_chaff (buf);
+
+            if (flag == 0)
+            {
+                continue;
+            }
+            copy (compuscript[i], buf, Columns);
+            litter (buf);
             //fputs (compuscript[i], stdout);
     }
     //while (compuscript[i-1] )
-    stupid_copy (compuscript[i], "END_OF_TEXT");
+    copy (compuscript[i], "END_OF_TEXT", Columns);
     return (i - 1);
 }
 
@@ -177,5 +208,129 @@ void ZOV (FILE *text, char agitation[])
         }
         //printf ("%c\n", ch);
 
+    }
+}
+
+int sep_grain_from_chaff(const char *harvest)
+{
+
+    char reference[2] = {' ', ' '};
+    if (*((short*)harvest) == *((short*)reference))
+    {
+        return (0);
+    }
+    
+    for (int i = 0; i < ver_width; i++)
+    {
+        
+        if (isalpha(harvest[i]))
+        {
+            return (1);
+        }
+
+        if (harvest[i] == '\n' || harvest[i] == '')
+        {
+            return (0);
+        }
+    }
+    return (0);
+}
+
+void litter (char *dump)
+{
+    for (int i = 0; i < ver_width; i++ )
+    {
+        dump[i] = '\0';
+    }
+}
+
+void copy (char* pathetic_parody,
+                  const char* unique_original, int Max_char_to_copy)
+{
+    int count = 0;
+    int i = 0;
+    int sh = 0;
+    int ch = 0;
+    int flag = 1;
+
+
+    for (; unique_original[count*8 + i*4 + sh*2 + ch] != '\0' && count*sizeof(long) + i*4 + sh*2+ ch < Max_char_to_copy  && flag != 0; )
+    {   
+        flag = 0;
+        if ((count+1)*sizeof(long) < Max_char_to_copy )
+        {
+            *(((long*)pathetic_parody) + count) = *(((long*)unique_original) + count);
+            //printf ("Итерация номер %d\n", count);
+            count++;
+            flag = 1;
+        }
+        
+        else
+        {   if ((count)*sizeof(long) + 2*sh + ch + 4*i + sizeof(int) < Max_char_to_copy )
+            {
+                *(((int*)pathetic_parody) + (2*count )) = *(((int*)unique_original) + (2*count));
+                //printf ("Зашел в инты\n");
+                i++;
+                flag = 1;
+            }
+            if (count*sizeof(long) + 4*i + sizeof(short) + 2*sh + ch < Max_char_to_copy)
+            {
+                *(((short*)pathetic_parody) + 4*count + 2*i) = *(((short*)unique_original) + 4*count + 2*i);
+                //printf ("Зашел в шорты\n");
+                sh++;
+                flag = 1;
+            }
+            //printf("Иду дальше\n");
+            if ((count)*sizeof(long) + 4*i + 2*sh + sizeof(char) < Max_char_to_copy )
+            {
+                *(((char*)pathetic_parody) + 8*count + 4*i + 2*sh) = *(((char*)unique_original) + 8*count + 4*i + 2*sh);
+                //printf ("Зашел в чары\n");
+                ch++;
+                flag = 1;
+            }
+            
+        }
+        
+
+        
+    }
+    pathetic_parody[Max_char_to_copy-1] = '\0'; 
+
+    //printf ("%d\n", count);
+
+    erase_evidence (pathetic_parody, (count) *sizeof(long)+i*4+sh*2+ch);
+
+/*
+    printf ("Жалкая пародия:        ");
+    for (int i = 0; i < Columns; i++)
+    {
+        printf ("%c", pathetic_parody[i]);
+    }
+    putchar ('\n');
+    printf ("Неповторимый оригинал: ");  
+    for (int i = 0; i < Columns; i++)
+    {
+        printf ("%c", unique_original[i]);
+    }
+    putchar ('\n');
+    */
+
+
+}
+
+void read_in_array (char *arr)
+{
+    char ch = 0;
+    for (int i = 0; (ch = getchar()) != '\n'; i ++ )
+    {
+        arr[i] = ch;
+    }
+}
+
+void erase_evidence(char *arr, int size)
+{
+    for (int i = size; i < Columns; i++)
+    {
+        arr[i] = '\0';
     }
 }
